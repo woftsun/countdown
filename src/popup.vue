8ce8b726d237e8a675dfad2fb8d37d7cca8f7bdc
戚生程 <qishengchengg@163.com>
2023-09-11 14:55:42 +0800

    docs: update

--- src/popup.vue
+++ src/popup.vue
@@ -65,7 +65,7 @@
     </div>
     <div
       v-if="saveStatus"
-      class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+      class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] mt-5">
       <div class="p-6">
         <h5
           class="mb-2 text-xl font-medium leading-tight text-neutral-800">
